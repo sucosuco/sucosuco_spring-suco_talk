e1ebcb14604795e3dbe521e84d5c3bd38a3de12a
ssop6403@gmail.com <ssop6403@gmail.com>
2021-05-22 21:15:39 +0900

    feat: 웹 소켓 설정 완료

--- sucotalk/src/main/kotlin/com/suco/sucotalk/chat/service/WebSocketChatHandler.kt
+++ sucotalk/src/main/kotlin/com/suco/sucotalk/chat/service/WebSocketChatHandler.kt
@@ -6,11 +6,10 @@ import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 
 @Component
-class WebSocketChatHandler : TextWebSocketHandler() {
+class WebSocketChatHandler() : TextWebSocketHandler() {
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         val payload = message.payload
-        print(payload)
         val textMessage = TextMessage("Welcome suco talk")
         session.sendMessage(textMessage)
     }
