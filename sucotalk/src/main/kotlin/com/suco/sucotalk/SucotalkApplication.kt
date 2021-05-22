3d3faa501817a42bcf54f262939f68282cad2c5e
dwl5 <ssop6403@gmail.com>
2021-05-22 13:08:50 +0900

    initial source commit

---
+++ sucotalk/src/main/kotlin/com/suco/sucotalk/SucotalkApplication.kt
@@ -0,0 +1,11 @@
+package com.suco.sucotalk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SucotalkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SucotalkApplication>(*args)
+}
---
+++ sucotalk/src/test/kotlin/com/suco/sucotalk/SucotalkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.suco.sucotalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SucotalkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
