851705bba7e9afd25ce1c5accc6bf2f857a56f17
dwl5 <ssop6403@gmail.com>
2021-05-29 11:42:20 +0900

    Auto stash before rebase of "origin/develop"

--- sucotalk/src/main/resources/schema.sql
+++ sucotalk/src/main/resources/schema.sql
@@ -1,21 +1,8 @@
 create table if not exists MEMBER
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    password varchar
-(
-    255
-) not null,
-    primary key
-(
-    id
-)
-    );
+    id bigint auto_increment not null,
+    name varchar(255) not null unique,
+    password varchar(255) not null,
+    primary key(id)
+);
+
