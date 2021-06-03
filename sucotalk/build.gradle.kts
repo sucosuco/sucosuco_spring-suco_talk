4370c17654a4c4a5770e241bf81ae6132ad1469f
dwl5 <ssop6403@gmail.com>
2021-06-03 11:21:46 +0900

    feat: ui추가 (#13)

--- sucotalk/build.gradle.kts
+++ sucotalk/build.gradle.kts
@@ -27,12 +27,20 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation ("org.springframework.boot:spring-boot-starter-freemarker")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
 	implementation ("com.h2database:h2")
+
+	implementation ("org.webjars.bower:bootstrap:4.3.1")
+	implementation ("org.webjars.bower:vue:2.5.16")
+	implementation ("org.webjars.bower:axios:0.17.1")
+	implementation ("org.webjars:sockjs-client:1.1.2")
+	implementation ("org.webjars:stomp-websocket:2.3.3-1")
+	implementation ("com.google.code.gson:gson:2.8.0")
 }
 
 tasks.withType<KotlinCompile> {
