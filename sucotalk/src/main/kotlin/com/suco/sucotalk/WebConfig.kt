3cf463df3ae3ea019e0f2b57180821bd34832870
ecsimsw <ecsimsw@gmail.com>
2021-06-14 13:49:18 +0900

    feat : CORS 설정

---
+++ sucotalk/src/main/kotlin/com/suco/sucotalk/WebConfig.kt
@@ -0,0 +1,14 @@
+package com.suco.sucotalk
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig :WebMvcConfigurer{
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+            .allowCredentials(true)
+    }
+}
