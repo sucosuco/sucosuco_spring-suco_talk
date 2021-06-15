748822c7d23896f3077c387458beba12314470fc
ecsimsw <ecsimsw@gmail.com>
2021-06-16 05:54:38 +0900

    refactor : 메시지 전송 시간 포맷팅

--- sucotalk/src/main/kotlin/com/suco/sucotalk/chat/repository/MessageDao.kt
+++ sucotalk/src/main/kotlin/com/suco/sucotalk/chat/repository/MessageDao.kt
@@ -4,8 +4,11 @@ import com.suco.sucotalk.chat.domain.Message
 import com.suco.sucotalk.member.domain.Member
 import com.suco.sucotalk.room.domain.Room
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Repository
 class MessageDao(private val jdbcTemplate: JdbcTemplate) {
@@ -22,21 +25,24 @@ class MessageDao(private val jdbcTemplate: JdbcTemplate) {
             ps
         }, keyHolder)
         val id = keyHolder.key!!.toLong()
-        return Message(id, message.sender, message.room, message.content, message.time)
+        val now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        return Message(id, message.sender, message.room, message.content, now)
     }
+    // TODO :: 시간 처리
 
     // TODO :: join 문 복습
     fun findByRoom(room: Room): List<Message> {
         val sql = "SELECT MS.id, MS.contents, MS.send_time, ME.id as member_id, ME.name, ME.password FROM MEMBER as ME JOIN MESSAGE as MS ON ME.id = MS.sender_id WHERE MS.room_id = ?"
+        return jdbcTemplate.query(sql, rowMapper(room), room.id)
+    }
 
-        return jdbcTemplate.query(sql, { rs, rn ->
-            Message(
-                id = rs.getLong("id"),
-                sender = Member(rs.getLong("member_id"), rs.getString("name"), rs.getString("password")),
-                room = room,
-                content = rs.getString("contents"),
-                time = rs.getTimestamp("send_time").toString()
-            )
-        }, room.id)
+    private fun rowMapper(room:Room) = RowMapper<Message> { rs, rn ->
+        Message(
+            id = rs.getLong("id"),
+            sender = Member(rs.getLong("member_id"), rs.getString("name"), rs.getString("password")),
+            room = room,
+            content = rs.getString("contents"),
+            time = rs.getTimestamp("send_time").toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        )
     }
 }
